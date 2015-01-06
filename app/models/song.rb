10ed3e4295c32feff90fd50075581cee5345da5a
Christina Stewart <cstewart@constantcontact.com>
2015-01-06 01:19:05 -0500

    one song on page

--- app/models/song.rb
+++ app/models/song.rb
@@ -5,14 +5,8 @@ class Song < ActiveRecord::Base
   CLIENT = Soundcloud.new(:client_id => '0bf253a58ca9b42631a739789bdc25c7',
       :client_secret => '0347fb41cfb4fd5e06d8aec6b97cc094')
 
-  def embed_info(url = self.url)
-    embed_info = CLIENT.get('/oembed', :url => url)
-    embed_info['html'].sub("400", "120").html_safe
-  end
-
-  def self.get_soundcloud_url(query)
-    tracks = CLIENT.get('/tracks', :q => query)
-    tracks[0][:permalink_url]
+  def soundcloud_player
+    SC::Track.embed_info(self.url)
   end
 
 end
---
+++ lib/sc/track.rb
@@ -0,0 +1,16 @@
+module SC
+  class Track
+    CLIENT = Soundcloud.new(:client_id => '0bf253a58ca9b42631a739789bdc25c7',
+        :client_secret => '0347fb41cfb4fd5e06d8aec6b97cc094')
+
+    def self.embed_info(url)
+      embed_info = CLIENT.get('/oembed', :url => url)
+      embed_info['html'].sub("400", "120").html_safe
+    end
+
+    def self.get_url(query)
+      tracks = CLIENT.get('/tracks', :q => query)
+      tracks[0][:permalink_url]
+    end
+  end
+end
